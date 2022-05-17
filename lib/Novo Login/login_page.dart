b70ce37b4710f2e9359a9ced4411f6781a9cd4ef
AndersonLucas30 <andersoncui3.0@gmail.com>
2022-05-17 12:43:15 -0300

    ATUALIZACOES

--- lib/Novo Login/login_page.dart
+++ lib/Novo Login/login_page.dart
@@ -1,8 +1,9 @@
-// ignore_for_file: prefer_const_constructors, unused_field, unused_import, unused_local_variable, avoid_print, prefer_typing_uninitialized_variables
+// ignore_for_file: prefer_const_constructors, unused_field, unused_import, unused_local_variable, avoid_print, prefer_typing_uninitialized_variables, deprecated_member_use
 
 import 'dart:convert';
 
 import 'package:castservice/FEED/feed_screen.dart';
+import 'package:castservice/FORMULARIO%20CADASTRO/form.dart';
 import 'package:castservice/Novo%20Login/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -40,13 +41,15 @@ class _LoginPageState extends State<LoginPage>{
                   labelText: 'e-mail'
                 ),
                 controller: _emailController,
-                keyboardType: TextInputType.emailAddress,
+                //keyboardType: TextInputType.emailAddress,
                 validator: (email){
                   if(email == null || email.isEmpty){
                     return'Por favor, digite seu e-mail';
                   } else if (!RegExp(
-                            r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
-                        .hasMatch(_emailController.text)) {
+                          r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                      .hasMatch(_emailController.text))
+                                     
+                      {                      
                       return 'Por favor, digite um e-mail correto';
                         }
                         return null;
@@ -57,12 +60,12 @@ class _LoginPageState extends State<LoginPage>{
                   labelText: 'senha'
                 ),
                 controller: _passwordController,
-                obscureText: true,
+               // obscureText: true,
                 keyboardType: TextInputType.text,
                 validator: (senha){
                   if(senha == null || senha.isEmpty){
                     return 'Por favor, digite sua senha';
-                  } else if (senha.length < 6){
+                  } else if (senha.length < 2){
                     return 'Por favor, digite uma senha maior que 6 caracteres';
                   }
                   return null;
@@ -76,7 +79,7 @@ class _LoginPageState extends State<LoginPage>{
                     currentFocus.unfocus();
                   }
                   if (deuCerto){
-                    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=> FeedScreen(),//HomePage(),
+                    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>HomePage(),
                     ),
                     );
                   }else{
@@ -88,6 +91,27 @@ class _LoginPageState extends State<LoginPage>{
               }, 
               child: Text('ENTRAR '),
               ),
+
+                FlatButton(
+                    onPressed:(){
+
+                    } ,
+                    child: Text(
+                      'esqueci a senha',
+                      style: TextStyle(color: Colors.black, fontSize: 15),
+                    ),
+                    ),
+
+
+                FlatButton(
+                    onPressed:(){
+                      Navigator.push(context, MaterialPageRoute(builder: (_) => FormCad()));
+                    } ,
+                    child: Text(
+                      'Criar conta',
+                      style: TextStyle(color: Colors.black, fontSize: 15),
+                    ),
+                    ),
           ],
           ),
         ),
@@ -103,7 +127,7 @@ class _LoginPageState extends State<LoginPage>{
 
   Future<bool> login() async{
     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-    var url = Uri.parse('https://castservice-app.herokuapp.com/users');
+    var url = Uri.parse('https://autenticacao-flutter.herokuapp.com/authenticate');
     var resposta  = await http.post(
     url,
     body: {
@@ -115,7 +139,7 @@ class _LoginPageState extends State<LoginPage>{
       await sharedPreferences.setString('token', "Token ${jsonDecode(resposta.body)['token']}");
       return true;
     } else{
-      print('errou');
+      print(jsonDecode(resposta.body));
       return false;
     }
   }
